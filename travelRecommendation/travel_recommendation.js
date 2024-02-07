e93b0c7881658f444ea10883d9e139380604979c
sunkarwar <sunkarwar@gmail.com>
2024-02-07 10:42:18 -0500

    functionality of search button

--- travelRecommendation/travel_recommendation.js
+++ travelRecommendation/travel_recommendation.js
@@ -1,11 +1,50 @@
-const allPlacesApi = fetch('./travel_recommendation_api.json');
-
-function retrievePlaces() {
-    let allPlaces;
-    allPlacesApi.then(data => data.json()).then(data => {
-        console.log(`data from arrow :${data}`);
-        allPlaces = data
-    });
-    console.log(allPlaces);
-    document.getElementById("div_placeCard").innerHTML = `<h1>${allPlaces}</h1>`;
+async function fetchData() {
+    try {
+        const fetchedDataResponse = await fetch('./travel_recommendation_api.json');
+        if (!fetchedDataResponse.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return (await fetchedDataResponse.json());
+    } catch (error) {
+        console.error('There was a problem fetching data from travel recommendation api :', error);
+        throw error; // Rethrow the error to propagate it to the caller
+    }
+}
+async function retrievePlaces() {
+    const allPlaces = await fetchData();
+    let filteredData;
+    const searchText = (document.getElementsByName('searchText')[0].value).toLowerCase();
+    // const BEACHES = ['BEACHES', 'beaches', 'beach', 'BEACH']
+    // Filter object based on keys that match the given value
+    switch (searchText) {
+        case 'beaches':
+        case 'beach':
+            filteredData = JSON.stringify(filterObjectByKey(allPlaces, 'beaches'));
+            break;
+        case 'temples':
+        case 'temple':
+            filteredData = JSON.stringify(filterObjectByKey(allPlaces, 'temples'));
+            break;
+        case 'countries':
+        case 'country':
+            filteredData = JSON.stringify(filterObjectByKey(allPlaces, 'countries'));
+            break;
+        default:
+            filteredData = JSON.stringify(allPlaces);
+    }
+    document.getElementById("div_placeCard").innerHTML = `<h1>${filteredData}</h1>`;
+}
+function clearAll(){
+    document.getElementById("div_placeCard").innerHTML = '';
+}
+// Function to filter object based on keys that match the given value
+function filterObjectByKey(obj, value) {
+    const filteredObject = {};
+    for (const [key, val] of Object.entries(obj)) {
+        if (key === value) {
+            console.log(`key : ${key} and value = ${val}`)
+            filteredObject[key] = val;
+        }
+    }
+    return filteredObject;
 }
